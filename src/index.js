bd5bf7e9159c1a558c13aea28c3b13b27b4593cc
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-25 14:55:50 +0200

    up

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';;
 
 const form = document.querySelector('#search-form')
-console.log(form)
 form.addEventListener('submit', onSearch)
 const container=document.querySelector('.gallery')
-console.log(container)
 const loadMore=document.querySelector('.load-more')
-console.log(loadMore)
 loadMore.addEventListener('click', onLoadMore)
+
 let page=1
 let input=''
 loadMore.hidden=true;
@@ -19,51 +17,50 @@ function onSearch(event){
     input = event.currentTarget.elements.searchQuery.value.trim();
      if(!input){
         container.innerHTML='';
-        loadMore.hidden=true;
+       loadMore.hidden = true;
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         return;
      }
-    
-    page=1
-    getPictures(input).then(data=>{
+        getPictures(input).then(data=>{
         if(data.hits.length===0){
             container.innerHTML='';
-            loadMore.hidden=true;
              Notify.failure('Sorry, there are no images matching your search query. Please try again.');
              return;
-        }
-        
+          }
+       
         page+=1,container.insertAdjacentHTML('beforeend', createMarkcup(data.hits)),
         Notify.success(`Hooray! We found ${data.totalHits} images.`),
         loadMore.hidden=false;
-        (console.log(data.totalHits));
+        (console.log(data.totalHits))
         
     })
         .catch(error=>console.log(error))
-    
-    
-    
+     
 }
-
-function onLoadMore(){
-    getPictures(input).then(data=>{page+=1,container.insertAdjacentHTML('beforeend', createMarkcup(data.hits)), console.log(page)}).catch(error=>console.log(error))
- 
-}
-
-
 
 
 const BASE_URL = 'https://pixabay.com/api/'
 
 function getPictures(pictureName){
      
-    const URL = `${BASE_URL}?key=35723548-55cce6d92fe2b0376e8aa06a2&q="${pictureName}"&image_type="photo"&orientation="horizontal"&safesearch="true"&per_page=40&page=${page}`;
+    const URL = `${BASE_URL}?key=35723548-55cce6d92fe2b0376e8aa06a2&q="${pictureName}"
+    &image_type="photo"&orientation="horizontal"&safesearch="true"&per_page=40&page=${page}`;
  return fetch(URL).then(resp=>{ 
-    if(!resp.ok){
+    if(!resp.ok){                                                                                                                                           
 throw new Error(resp.statusText)
     }
     return resp.json();
 })
 
+}
+function onLoadMore(){
+  getPictures(input).then(data => {
+    
+    page += 1, container.insertAdjacentHTML('beforeend', createMarkcup(data.hits));
+   
+    
+    console.log(page)}).catch(error=>console.log(error))
+    
 }
 
 function createMarkcup (arr){
